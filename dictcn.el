44258b1a06ddc5ff1f944ed5fbc9606719b6f8cd
gucong <gucong43216@gmail.com>
2011-09-16 22:39:11 -0500

    new file:   dictcn.el

---
+++ dictcn.el
@@ -0,0 +1,18 @@
+;; If you want to use dictcn-script in emacs,
+;; put this file into your load-path and the following into your
+;; ~/.emacs:
+;;    (require 'dictcn)
+;;    (define-key global-map (kbd "C-c d") 'dictcn-lookup)
+
+(provide 'dictcn)
+(defun dictcn-lookup ()
+  (interactive)
+  (let* ((guess (or (and transient-mark-mode mark-active
+                         (buffer-substring-no-properties
+                          (region-beginning)
+                          (region-end)))
+                    (current-word nil t)))
+         (word (read-string (format "dict.cn (default: %s): " guess)
+                            nil nil guess)))
+    (shell-command (format "dictcn -w %s" word)
+                   "*dictcn*")))
