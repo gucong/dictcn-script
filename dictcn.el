833755fda5179c9121d8f814bdc07d998c81a3f0
gucong <gucong43216@gmail.com>
2011-09-18 19:13:13 -0500

    emacs support

--- dictcn.el
+++ dictcn.el
@@ -5,8 +5,11 @@
 ;;    (define-key global-map (kbd "C-c d") 'dictcn-lookup)
 
 (provide 'dictcn)
-(defun dictcn-lookup ()
-  (interactive)
+(defun dictcn-lookup (pronounce-p)
+    "Prompt for a word to look up in dict.cn.
+When provided with a prefix argument, pronounce the word
+`sdcv-dictionary-list' before search."
+  (interactive "P")
   (let* ((guess (or (and transient-mark-mode mark-active
                          (buffer-substring-no-properties
                           (region-beginning)
@@ -14,5 +17,5 @@
                     (current-word nil t)))
          (word (read-string (format "dict.cn (default: %s): " guess)
                             nil nil guess)))
-    (shell-command (format "dictcn -w \"%s\"" word)
+    (shell-command (concat "dictcn -w \"" word "\"" (if pronounce-p " -p " ""))
                    "*dictcn*")))
